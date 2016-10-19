d8431e9af4b88e557c2ddba0e99ae81042cbc26c
ygandy <yygandy@gmail.com>
2016-10-19 09:55:38 -0400

    updated Deposit Controller to include new functionality and to fix add/submit button

--- src/main/java/co/grandcircus/dreamsetter/controller/DepositController.java
+++ src/main/java/co/grandcircus/dreamsetter/controller/DepositController.java
@@ -1,6 +1,7 @@
 package co.grandcircus.dreamsetter.controller;
 import java.util.List;
-
+import java.sql.Timestamp;
+import java.util.Date;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,6 +32,53 @@ public class DepositController {
 	private DepositDao depositDao;
 	
 	/**
+	 * Display Total of deposit amounts for one goal 
+	 */
+	@RequestMapping(value = "/deposit-details", method = RequestMethod.GET)
+	public String getTotalDeposits(Model model, @RequestParam (value= "depositAmount", required = false)Double depositAmount){
+		
+		model.addAttribute("depositAmount", depositDao.getTotalDeposits());
+		
+		logger.info("GET /deposit/" + depositAmount + " -> deposit-details.jsp");
+		
+		return "deposit-details";
+	}
+
+
+	/**
+	 * Add/Redirect to save new deposit
+	 */
+	@RequestMapping(value = "/deposit-details", method = RequestMethod.POST)
+	//public String createDeposit(Model model, @RequestParam (value= "depositAmount", required = false)Double depositAmount){
+	public String createDeposit(Deposit depositAmount, Model model) {
+		depositAmount.setGoalId(1);
+		//depositAmount.setCurrentDate("");
+		depositDao.addDeposit(depositAmount);
+		//model.addAttribute("depositAmount", depositAmount);
+		model.asMap().clear();
+	
+		logger.info("POST /deposit-details -> redirect to /deposit-details.jsp");
+		return "redirect:/deposit-details";
+		
+	}
+	
+}//the end!
+
+//	/**
+//	 * Display one deposit
+//	 * @throws Exception 
+//	 */
+//	@RequestMapping(value = "/deposit-details{depositId}", method = RequestMethod.GET)
+//	public String displayDeposit(@PathVariable int depositId, Model model){
+//		model.addAttribute("depositId", depositId);
+//		model.addAttribute("depositAmount", depositDao.getDepositAmount(depositId));
+//		
+//		logger.info("GET /deposit/" + depositId + " -> deposit-details.jsp");
+//		//logger.info("GET /deposit/" + depositId + " -> goalsdashboard.jsp");
+//		return "deposit-details";
+//	}
+	
+	/**
 	 * Selects the deposit view and display all deposits
 	 *  
 	 */
@@ -53,75 +101,6 @@ public class DepositController {
 //	}
 //
 	
-	/**
-	 * Display Total of deposit amounts for one goal 
-	 */
-	@RequestMapping(value = "/deposit-details", method = RequestMethod.GET)
-	public String getTotalDeposits(Model model, @RequestParam (value= "depositAmount", required = false)Double depositAmount){
-		//model.addAttribute("depositId", depositId);
-		model.addAttribute("depositAmount", depositDao.getTotalDeposits());
-		
-		logger.info("GET /deposit/" + depositAmount + " -> deposit-details.jsp");
-		
-		return "deposit-details";
-	}
-
-//	/**
-//	 * Display one deposit
-//	 * @throws Exception 
-//	 */
-//	@RequestMapping(value = "/deposit-details{depositId}", method = RequestMethod.GET)
-//	public String displayDeposit(@PathVariable int depositId, Model model){
-//		model.addAttribute("depositId", depositId);
-//		model.addAttribute("depositAmount", depositDao.getDepositAmount(depositId));
-//		
-//		logger.info("GET /deposit/" + depositId + " -> deposit-details.jsp");
-//		//logger.info("GET /deposit/" + depositId + " -> goalsdashboard.jsp");
-//		return "deposit-details";
-//	}
-	
-	/**
-	 * Save/update one deposit
-	 * @throws NotFoundException 
-	 */
-	@RequestMapping(value = "/deposit/{id}", method = RequestMethod.POST)
-	public String saveDeposit(@PathVariable int id, Deposit deposit, Model model) throws NotFoundException {
-		depositDao.updateDeposit(id, deposit);
-		model.addAttribute("id", id);
-		model.addAttribute("deposit", deposit);
-		
-		logger.info("POST /deposit/" + id + " -> deposit.jsp");
-		return "deposit";
-	}
-	
-	/**
-	 * Delete one deposit
-	 * @throws NotFoundException 
-	 */
-	@RequestMapping(value = "/Deposits/{id}/delete", method = RequestMethod.POST)
-	public String deleteDeposit(@PathVariable int id, Model model) throws NotFoundException {
-		depositDao.deleteDeposit(id);
-		model.asMap().clear();
-		
-		logger.info("POST /deposit/" + id + "/delete -> redirect to /deposit");
-		return "redirect:/deposit";
-	}
-	
-	
-	/**
-	 * Add/Redirect to save new deposit
-	 */
-	@RequestMapping(value = "/deposit-details", method = RequestMethod.POST)
-	public String createDeposit(Deposit depositAmount, Model model) {
-		depositDao.addDeposit(depositAmount);
-		model.asMap().clear();
-		System.out.println(depositAmount);
-		logger.info("POST /deposit-details -> redirect to /deposit-details.jsp");
-		return "redirect:/deposit-details";
-		
-	}
-	
-}//the end
 	
 		
 	
